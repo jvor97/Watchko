b98e213e6b457cc27f203e9e29370155d5ffa48b
Vorcakova <julia.vorcakova@accenture.com>
2019-10-14 13:43:55 +0200

    API-full movie

--- src/containers/FullMovie/FullMovie.js
+++ src/containers/FullMovie/FullMovie.js
@@ -1,33 +1,65 @@
 import React, {Component} from 'react';
 
 import FullMovieButtons from '../../components/FullMovieButtons/FullMovieButtons';
+import axios from 'axios';
 
 class FullMovie extends Component {
 
     state={
-        title: '',
-        originalTitle: '',
-        genres: '',
-        release_date: '',
-        runtime: '',
-        overview: ''
+        selectedMovie: null,
+        previousID: null
+    }
+
+    componentDidMount(){
+        this.loadMovieDataHandler();
+    }
+
+    componentDidUpdate(){
+        this.loadMovieDataHandler();
+    }
+
+    loadMovieDataHandler(){
+        if (this.props.match.params.id) {
+            if (this.props.match.params.id != this.state.previousID) {
+                axios.get('https://api.themoviedb.org/3/movie/' + this.props.match.params.id + '?api_key=65777f92529c3462f958232f137b357f&language=en-US&page=1&fbclid=IwAR3WdGpp9ZHMyGn4Vyni4MFF0hpc-Kfvyyj9PLnyueheoQ0o3YIPcmSL5Dk&language=en-US').then(response =>
+                this.setState({
+                selectedMovie: response.data,
+                previousID: this.props.match.params.id
+                }))
+            }
+        }
     }
 
     render(){
-        return(
-            <div className='card bg-dark'>
-            <img className="card-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/90/HeathJoker.png/220px-HeathJoker.png" alt="Card image"/>
+        let movie = <p>Please selest a movie</p>
+        if (this.props.match.params.id) {
+            movie = <p>Loading please wait...</p>
+        }
+        if (this.state.selectedMovie) {
+            let genres = this.state.selectedMovie.genres.map(genre => genre.name + ' | ');
+            console.log(genres);
+
+           movie =(<div className='card bg-dark'>
+                <img className="card-img" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/90/HeathJoker.png/220px-HeathJoker.png" alt="Card image"/>
                 <div className="card-img-overlay">
-                <h1 className='card-title'>This is title</h1>
-                <h3>genres</h3>
+                <h1 className='card-title'>{this.state.selectedMovie.title}</h1>
+                <h5>{genres}</h5>
                 <div className="card-text">
-                    <p>release_date</p>
-                    <p>runtime</p>
+                    <p>{this.state.selectedMovie.release_date}</p>
+                    <p>{this.state.selectedMovie.runtime + ' min'}</p>
                 </div>
                 <div className="card-text">
-                    overview
+                    {this.state.selectedMovie.overview}
                 </div>
                 </div>
+                </div>)
+        }
+
+   
+        
+        return(
+            <div>
+            {movie}
                 <FullMovieButtons/>
             </div>
             
