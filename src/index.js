9b1e3d7aa97d7b32ba119e81f94bd80db26c2f19
Julia Vorcakova <julia.vorcakova1@gmail.com>
2019-10-26 17:03:13 +0200

    redux dev tool=extension to chrome

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore,applyMiddleware, combineReducers} from 'redux';
+import {createStore,applyMiddleware, combineReducers, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
@@ -13,12 +13,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ApiReducer from './store/reducers/APIdata';
 import DisplayReducer from './store/reducers/displayEl';
 
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const reducer = combineReducers({
     api: ApiReducer,
     displayEl: DisplayReducer,
 })
-let store = createStore(reducer, applyMiddleware(thunk));
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
